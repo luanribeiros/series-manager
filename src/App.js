5a579365abcb71e72214ae1b64a56897cd6db1f3
Luan Ribeiro <luanribeiros@gmail.com>
2018-04-24 22:32:15 -0300

    criando arquivo api e melhorando nosso código

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
     }
   }
   componentDidMount() {
-    axios.get('http://localhost:3001/genres')
+    axios.get()
       .then((res) => {
         console.log(res)
       })
