2404d0941ae09657881683dcc52b67efc03ae5bc
Luan Ribeiro <luanribeiros@gmail.com>
2018-04-27 21:49:30 -0300

    criando Component Home

--- src/App.js
+++ src/App.js
@@ -32,51 +32,28 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="App">
-        <nav className="navbar navbar-default navbar-fixed-top" role="navigation">
-          <div className="container">
-            <div className="navbar-header page-scroll">
-              <a className="navbar-brand page-scroll" href="#page-top">
-                  <img src="images/logo.png" height="30" />
-              </a>
-            </div>
-      
-            <div className="collapse navbar-collapse navbar-ex1-collapse">
-              <ul className="nav navbar-nav">
-                <li>
-                  <a href="">Menu item</a>
-                </li>
-              </ul>
-            </div>
-      
-          </div>
-        </nav>
-      
-      
-        <section id="intro" className="intro-section">
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-12">
-                <h1><img src="images/logo.png" /></h1>
-                <p>Nunca mais esqueça uma série que você assistiu ou que alguém lhe indicou.</p>
+      <Router>
+        <div className="App">
+          <nav className="navbar navbar-default navbar-fixed-top" role="navigation">
+            <div className="container">
+              <div className="navbar-header page-scroll">
+                <a className="navbar-brand page-scroll" href="#page-top">
+                    <img src="images/logo.png" height="30" />
+                </a>
               </div>
+        
+              <div className="collapse navbar-collapse navbar-ex1-collapse">
+                <ul className="nav navbar-nav">
+                  <li>
+                    <a href="">Menu item</a>
+                  </li>
+                </ul>
+              </div>
+        
             </div>
-          </div>
-        </section>
-
-        <section>
-          {
-            this.state.isLoading && <span> Aguarde, carregando...</span>
-          }
-          {
-            !this.state.isLoading && 
-            <div> 
-              Ver séries do gênero:
-              { this.state.genres.map(this.renderGenreLink)}
-            </div>
-          }
-        </section>
-      </div>
+          </nav>
+        </div>
+      </Router>
     )
   }
 }
