a4165bc8d247b643a2ff5695461db6e6e01c339e
Luan Ribeiro <luanribeiros@gmail.com>
2018-05-05 12:32:49 -0300

    criando estrutura dentro dos gêneros

--- src/Series.js
+++ src/Series.js
@@ -9,6 +9,27 @@ class Series extends Component {
     return (
         <section id="intro" className="intro-section">
           <h1> Series {this.props.match.params.genre}</h1>
+
+          <div id="series" className="row list-group">
+            <div className="item  col-xs-4 col-lg-4">
+              <div className="thumbnail">
+                <img className="group list-group-image" src="http://placehold.it/400x250/000/fff" alt="" />
+                <div className="caption">
+                  <h4 className="group inner list-group-item-heading">
+                    How I met your mother</h4>
+                  <div className="row">
+                    <div className="col-xs-12 col-md-6">
+                      <p className="lead">
+                        AÇÃO</p>
+                    </div>
+                    <div className="col-xs-12 col-md-6">
+                      <a className="btn btn-success" href="">Gerenciar</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </section>
     )
   }
