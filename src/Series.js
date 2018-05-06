c3c0210a180289168d5c1232fa578e957692565a
Luan Ribeiro <luanribeiros@gmail.com>
2018-05-06 08:51:19 -0300

    colocando a estrutura das series em uma função

--- src/Series.js
+++ src/Series.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react'
 
 class Series extends Component {
-  constructor(props) {
-    super(props) 
+  renderSeries() {
+    return (
+      <div className="item  col-xs-4 col-lg-4">
+        <div className="thumbnail">
+          <img className="group list-group-image" src="http://placehold.it/400x250/000/fff" alt="" />
+          <div className="caption">
+            <h4 className="group inner list-group-item-heading">
+              How I met your mother</h4>
+            <div className="row">
+              <div className="col-xs-12 col-md-6">
+                <p className="lead">
+                  AÇÃO</p>
+              </div>
+              <div className="col-xs-12 col-md-6">
+                <a className="btn btn-success" href="">Gerenciar</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
   }
 
   render() {
@@ -11,24 +30,7 @@ class Series extends Component {
           <h1> Series {this.props.match.params.genre}</h1>
 
           <div id="series" className="row list-group">
-            <div className="item  col-xs-4 col-lg-4">
-              <div className="thumbnail">
-                <img className="group list-group-image" src="http://placehold.it/400x250/000/fff" alt="" />
-                <div className="caption">
-                  <h4 className="group inner list-group-item-heading">
-                    How I met your mother</h4>
-                  <div className="row">
-                    <div className="col-xs-12 col-md-6">
-                      <p className="lead">
-                        AÇÃO</p>
-                    </div>
-                    <div className="col-xs-12 col-md-6">
-                      <a className="btn btn-success" href="">Gerenciar</a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+            { this.renderSeries()}
           </div>
         </section>
     )
