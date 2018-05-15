e53bb0b9f19baee338550aacaec88ea60c82ff79
Luan Ribeiro <luanribeiros@gmail.com>
2018-05-14 23:19:05 -0300

    aplicando a exclusão do elemento

--- src/Api.js
+++ src/Api.js
@@ -8,11 +8,13 @@ const api = axios.create({
 export const loadGenres = () => api.get('genres')
 export const saveSeries = (NewSeries) => api.post('series', NewSeries)
 export const loadSeriesByGenre = (genre) => api.get('series?genre='+genre)
+export const deleteSeries = (id) => api.delete('series/'+id)
 
 const apis = {
   loadGenres,
   saveSeries,
-  loadSeriesByGenre
+  loadSeriesByGenre,
+  deleteSeries
 }
 
 export default apis
--- src/Series.js
+++ src/Series.js
@@ -32,8 +32,10 @@ class Series extends Component {
   }
 
   deleteSeries(id) {
-
+    Api.deleteSeries(id)
+      .then((res) => console.log(res))
   }
+  
   renderSeries(series) {
     return (
       <div className="item  col-xs-4 col-lg-4">
@@ -49,8 +51,8 @@ class Series extends Component {
               </div>
               <div className="col-xs-12 col-md-6">
                 <a className="btn btn-success" href="">Gerenciar</a>
-                <a className="btn btn-success" href="" onClick={ }>Excluir</a>
-                </div>
+                <a className="btn btn-success" href="" onClick={ this.deleteSeries(series.id) }>Excluir</a>
+              </div>
             </div>
           </div>
         </div>
